b522b936dd9612142144fc2d4a556bba36f85eb9
Raj Joshi <geeky2307@gmail.com>
2019-06-02 09:37:15 -0400

    updated lab 6 --> completed

--- Lab 6/MPLABTemplate.asm
+++ Lab 6/MPLABTemplate.asm
@@ -1,86 +1,78 @@
-; ALab 6
-; Raj Joshi & Arihant GUPTA
+; Lab 6
+; Raj Joshi & Arihant Gupta
 ; 5/22/19
 
 #include <p16f887.inc>
-
-key_pressed equ 0x20
 
 	ORG 0
 	GOTO main
 	ORG 4
 	GOTO isr
 
-
 main:
-	BANKSEL ANSELH
-	CLRF ANSELH
-	
-	BANKSEL TRISB	
-	MOVLW b'11110000' ;0-3 is output 4-7 is input
+	BANKSEL OPTION_REG
+	BCF OPTION_REG, 7
+	BANKSEL TRISB
+	MOVLW b'00001111' ;4-7 are outputs (rows) 0-3 are inputs (columns)
 	MOVWF TRISB
-
+	BANKSEL WPUB
+	MOVWF WPUB
 	BANKSEL ANSEL
 	CLRF ANSEL
-
-	BANKSEL TRISC
-	CLRF TRISC
-
+	BANKSEL TRISD
+	CLRF TRISD	
 	BANKSEL PORTB
-	CLRF PORTB
-	CLRF PORTC
-	CLRF key_pressed
+	MOVLW 0xFF
+	MOVWF PORTB
+	BANKSEL PORTD
+	CLRF PORTD
 
-test_keys:;common anode, msb is g lsb is a
-	MOVF key_pressed, 0
-	
-	BSF PORTB, 0 ;testing column 4
-	BTFSC PORTB, 4 ;check row 4
-	MOVLW b'10100001';0x0D
-	BTFSC PORTB, 5 ;check row 3
-	MOVLW b'10000110';0x0E
-	BTFSC PORTB, 6 ;check row 2
-	MOVLW b'10001110';0x0F
-	BTFSC PORTB, 7 ;check row 1
-	MOVLW b'11000000';0x00
-	BCF PORTB, 0
-	
-	BSF PORTB, 1 ;testing column 3
-	BTFSC PORTB, 4 ;check row 4
-	MOVLW b'11000110';0x0C
-	BTFSC PORTB, 5 ;check row 3
-	MOVLW b'10011000';0x09
-	BTFSC PORTB, 6 ;check row 2
-	MOVLW b'10000000';0x08
-	BTFSC PORTB, 7 ;check row 1
-	MOVLW b'11111000';0x07
-	BCF PORTB, 1
-
-	BSF PORTB, 2 ;testing column 2
-	BTFSC PORTB, 4 ;check row 4
-	MOVLW b'10000011';0x0B
-	BTFSC PORTB, 5 ;check row 3
-	MOVLW b'10000010';0x06
-	BTFSC PORTB, 6 ;check row 2
-	MOVLW b'10010010';0x05
-	BTFSC PORTB, 7 ;check row 1
-	MOVLW b'10011001';0x04
-	BCF PORTB, 2
-
-	BSF PORTB, 3 ;testing column 1
-	BTFSC PORTB, 4 ;check row 4
-	MOVLW b'10001000';0x0A
-	BTFSC PORTB, 5 ;check row 3
-	MOVLW b'10110000'0x03
-	BTFSC PORTB, 6 ;check row 2
-	MOVLW b'10100100';0x02
-	BTFSC PORTB, 7 ;check row 1
+test_keys:;common anode, msb-1 is g lsb is a
+	BCF PORTB, 7 ;testing column 1
+	BTFSS PORTB, 3 
 	MOVLW b'11001111';0x01
-	BCF PORTB, 3
-
-	MOVWF key_pressed
-	MOVWF PORTC
+	BTFSS PORTB, 2
+	MOVLW b'10011001';0x04
+	BTFSS PORTB, 1
+	MOVLW b'11111000';0x07
+	BTFSS PORTB, 0
+	MOVLW b'11000000';0x00
+	BSF PORTB, 7
 	
+	BCF PORTB, 6 ;testing column 2
+	BTFSS PORTB, 3
+	MOVLW b'10100100';0x02
+	BTFSS PORTB, 2
+	MOVLW b'10010010';0x05
+	BTFSS PORTB, 1
+	MOVLW b'10000000'
+	BTFSS PORTB, 0
+	MOVLW b'10001110';0x0F
+	BSF PORTB, 6
+
+	BCF PORTB, 5 ;testing column 3
+	BTFSS PORTB, 3
+	MOVLW b'10110000';0x03
+	BTFSS PORTB, 2
+	MOVLW b'10000010';0x06
+	BTFSS PORTB, 1
+	MOVLW b'10011000';0x09
+	BTFSS PORTB, 0
+	MOVLW b'10000110';0x0E
+	BSF PORTB, 5
+
+	BCF PORTB, 4 ;testing column 4
+	BTFSS PORTB, 3
+	MOVLW b'10001000';0x0A
+	BTFSS PORTB, 2
+	MOVLW b'10000011';0x0B
+	BTFSS PORTB, 1
+	MOVLW b'11000110';0x0C
+	BTFSS PORTB, 0
+	MOVLW b'10100001';0x0D
+	BSF PORTB, 4
+
+	MOVWF PORTD
 	GOTO test_keys
 
 isr:
